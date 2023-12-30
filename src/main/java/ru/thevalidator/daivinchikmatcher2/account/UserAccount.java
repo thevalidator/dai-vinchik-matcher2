ac3d795bc9ffea55914c972c2c01c7bdb989229a
illusion <the.validator@yandex.ru>
2023-12-30 23:58:36 +0300

    add apache dependencies

---
+++ src/main/java/ru/thevalidator/daivinchikmatcher2/account/UserAccount.java
@@ -0,0 +1,27 @@
+package ru.thevalidator.daivinchikmatcher2.account;
+
+public class UserAccount {
+    private String name;
+    private String token;
+
+    public UserAccount(String name, String token) {
+        this.name = name;
+        this.token = token;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+}
