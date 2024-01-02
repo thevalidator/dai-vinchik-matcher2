c9c39e9ccdbb7839995a6c08507310b1efe1317d
illusion <the.validator@yandex.ru>
2024-01-02 22:53:00 +0300

    add userId

--- src/main/java/ru/thevalidator/daivinchikmatcher2/vk/custom/actor/CustomUserActor.java
+++ src/main/java/ru/thevalidator/daivinchikmatcher2/vk/custom/actor/CustomUserActor.java
@@ -3,7 +3,19 @@ package ru.thevalidator.daivinchikmatcher2.vk.custom.actor;
 import com.vk.api.sdk.client.actors.UserActor;
 
 public class CustomUserActor extends UserActor {
+
+    private Long userId = null;
+
     public CustomUserActor(String accessToken) {
         super(null, accessToken);
+    }
+
+    public void setId(Long userId) {
+        this.userId = userId;
+    }
+
+    @Override
+    public Long getId() {
+        return userId;
     }
 }
