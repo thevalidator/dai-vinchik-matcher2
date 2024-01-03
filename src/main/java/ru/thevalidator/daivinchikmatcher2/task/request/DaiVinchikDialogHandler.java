052e2c087cc90ca245ff9c1597f7c199fa987456
illusion <the.validator@yandex.ru>
2024-01-04 00:14:11 +0300

    add stats and dialog answer service

--- src/main/java/ru/thevalidator/daivinchikmatcher2/task/request/DaiVinchikDialogHandler.java
+++ src/main/java/ru/thevalidator/daivinchikmatcher2/task/request/DaiVinchikDialogHandler.java
@@ -4,36 +4,45 @@ import com.vk.api.sdk.client.VkApiClient;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import ru.thevalidator.daivinchikmatcher2.account.UserAccount;
-import ru.thevalidator.daivinchikmatcher2.task.Task;
-import ru.thevalidator.daivinchikmatcher2.vk.custom.actor.CustomUserActor;
-import ru.thevalidator.daivinchikmatcher2.vk.dto.MessageWithKeyboard;
 import ru.thevalidator.daivinchikmatcher2.service.CaseMatcher;
+import ru.thevalidator.daivinchikmatcher2.service.DaiVinchikDialogAnswerService;
 import ru.thevalidator.daivinchikmatcher2.service.DaiVinchikMessageService;
 import ru.thevalidator.daivinchikmatcher2.service.impl.CaseMatcherImpl;
+import ru.thevalidator.daivinchikmatcher2.service.impl.DaiVinchikDialogAnswerServiceImpl;
 import ru.thevalidator.daivinchikmatcher2.service.impl.DaiVinchikMessageServiceImpl;
+import ru.thevalidator.daivinchikmatcher2.task.Task;
+import ru.thevalidator.daivinchikmatcher2.vk.custom.actor.CustomUserActor;
+import ru.thevalidator.daivinchikmatcher2.vk.dto.DaiVinchikDialogAnswer;
+import ru.thevalidator.daivinchikmatcher2.vk.dto.MessageWithKeyboard;
 
 public class DaiVinchikDialogHandler implements Task {
+
     private static final Logger LOG = LoggerFactory.getLogger(DaiVinchikDialogHandler.class);
+    private final CustomUserActor actor;
+    private final DaiVinchikMessageService messageService;
     private static final Integer LAST_MESSAGES_COUNT = 5;
     private final VkApiClient vk;
     private final UserAccount account;
-    private final CustomUserActor actor;
-    private final DaiVinchikMessageService service;
     private final CaseMatcher matcher;
+    private final DaiVinchikDialogAnswerService answerService;
+    private final Statistic stats;
 
 
     public DaiVinchikDialogHandler(VkApiClient vk, UserAccount account) {
         this.vk = vk;
         this.account = account;
         this.actor = new CustomUserActor(account.getToken());
-        service = new DaiVinchikMessageServiceImpl(vk, actor);
+        messageService = new DaiVinchikMessageServiceImpl(vk, actor);
+        answerService = new DaiVinchikDialogAnswerServiceImpl();
         matcher = new CaseMatcherImpl();
+        stats = new Statistic();
     }
 
     @Override
     public void run() {
 
-        MessageWithKeyboard msg = service.getDaiVinchikLastMessage();
+        MessageWithKeyboard msg = messageService.getDaiVinchikLastMessage();
+        DaiVinchikDialogAnswer answer = answerService.findAnswer(msg);
 
     }
 
@@ -41,4 +50,9 @@ public class DaiVinchikDialogHandler implements Task {
     public void stop() {
         throw new UnsupportedOperationException("Not supported yet");
     }
+
+}
+
+class Statistic {
+
 }
