ff782c8c845b3105d8da8662572ae32df0ede7c9
illusion <the.validator@yandex.ru>
2024-01-07 00:01:39 +0300

    add case types draft

---
+++ src/main/java/ru/thevalidator/daivinchikmatcher2/service/CaseType.java
@@ -0,0 +1,21 @@
+package ru.thevalidator.daivinchikmatcher2.service;
+
+public enum CaseType {
+
+    START_MESSAGE,
+    TOO_MANY_LIKES,
+    LIKED_BY_SOMEONE,// + AFTER PROFILE
+    WANT_TO_MEET,
+    MUTUAL_LIKE,
+    LONG_TIME_AWAY,
+    ADVERTISEMENT,
+    PROFILE,
+    PROFILE_AFTER_WRONG_ANSWER,
+    GOT_LIKES,
+    SKIP_BUTTON,
+    SKIP_TEXT,
+    CAN_NOT_CONTINUE,
+    UNKNOWN,
+    CAPTCHA
+
+}
