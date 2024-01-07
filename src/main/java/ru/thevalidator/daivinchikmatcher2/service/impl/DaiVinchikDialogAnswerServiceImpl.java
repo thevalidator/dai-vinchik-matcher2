29e110895bfc3dbae7949d606b714d557b44d201
illusion <the.validator@yandex.ru>
2024-01-07 22:03:47 +0300

    add support new case type and missing messages counter

--- src/main/java/ru/thevalidator/daivinchikmatcher2/service/impl/DaiVinchikDialogAnswerServiceImpl.java
+++ src/main/java/ru/thevalidator/daivinchikmatcher2/service/impl/DaiVinchikDialogAnswerServiceImpl.java
@@ -13,10 +13,9 @@ import java.util.Scanner;
 public class DaiVinchikDialogAnswerServiceImpl implements DaiVinchikDialogAnswerService {
 
     private static final Logger LOG = LoggerFactory.getLogger(DaiVinchikDialogAnswerServiceImpl.class);
-
     private final CaseMatcher matcher;
-
     private int counter = 0;
+    private int lastMessageId = 0;
 
     public DaiVinchikDialogAnswerServiceImpl() {
         matcher = new CaseMatcherImpl();
@@ -28,10 +27,31 @@ public class DaiVinchikDialogAnswerServiceImpl implements DaiVinchikDialogAnswer
 
     @Override
     public DaiVinchikDialogAnswer findAnswer(MessageAndKeyboard data) {
-        LOG.info("Iteration: {}", ++counter);
+        LOG.info("Iteration: {}, missed {}", ++counter, (data.getMessage().getConversationMessageId() - lastMessageId + 2)); //@TODO: transfer to main service class
+        lastMessageId = data.getMessage().getConversationMessageId();
         DaiVinchikDialogAnswer answer = new DaiVinchikDialogAnswer();
         CaseType type = matcher.detectCase(data);
-        if (type.equals(CaseType.UNKNOWN)) {
+        if (type.equals(CaseType.PROFILE)) {
+            LOG.debug("PROFILE: {}", data);
+            String text = data.getKeyboard().getButtons().get(0).get(0).getAction().getPayload();
+            answer.setText(text);
+        } else if (type.equals(CaseType.WARNING)) {
+            LOG.debug("WARNING: {}", data);
+            String text = data.getKeyboard().getButtons().get(0).get(0).getAction().getPayload();
+            answer.setText(text);
+        } else if (type.equals(CaseType.LOCATION)) {
+            LOG.debug("LOCATION: {}", data);
+            String text = data.getKeyboard().getButtons().get(1).get(0).getAction().getPayload();
+            answer.setText(text);
+        } else if (type.equals(CaseType.ADS_DV)) {
+            LOG.debug("ADS_DV: {}", data);
+            String text = data.getKeyboard().getButtons().get(0).get(1).getAction().getPayload();
+            answer.setText(text);
+        } else if (type.equals(CaseType.ADVICE)) {
+            LOG.debug("ADVICE: {}", data);
+            String text = data.getKeyboard().getButtons().get(0).get(1).getAction().getPayload();
+            answer.setText(text);
+        } else {
             LOG.debug("UNKNOWN STATE: {}", data);
             System.out.println("Кто-то хочет познакомиться");
             System.out.println("\n>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>" +
@@ -43,9 +63,6 @@ public class DaiVinchikDialogAnswerServiceImpl implements DaiVinchikDialogAnswer
             System.out.println();
             LOG.debug("STATE ANSWER: {}", input);
             answer.setText(input);
-        } else if (type.equals(CaseType.PROFILE)) {
-            LOG.debug("PROFILE: {}", data);
-            answer.setText("1");
         }
 
         return answer;
