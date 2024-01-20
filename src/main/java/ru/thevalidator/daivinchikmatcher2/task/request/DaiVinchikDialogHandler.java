a269c7e8769c21299b03642955a3577ac44413da
illusion <the.validator@yandex.ru>
2024-01-21 00:32:05 +0300

    add aproximately number of likes/dislikes counters

--- src/main/java/ru/thevalidator/daivinchikmatcher2/task/request/DaiVinchikDialogHandler.java
+++ src/main/java/ru/thevalidator/daivinchikmatcher2/task/request/DaiVinchikDialogHandler.java
@@ -10,6 +10,7 @@ import org.springframework.stereotype.Component;
 import ru.thevalidator.daivinchikmatcher2.config.settings.Settings;
 import ru.thevalidator.daivinchikmatcher2.exception.CanNotContinueException;
 import ru.thevalidator.daivinchikmatcher2.exception.TooManyLikesForToday;
+import ru.thevalidator.daivinchikmatcher2.service.CaseType;
 import ru.thevalidator.daivinchikmatcher2.service.DaiVinchikDialogAnswerService;
 import ru.thevalidator.daivinchikmatcher2.service.DaiVinchikMessageService;
 import ru.thevalidator.daivinchikmatcher2.service.DaiVinchikMissedMessageService;
@@ -71,8 +72,15 @@ public class DaiVinchikDialogHandler implements Task {
                 SendMessageResultResponse resultRs = messageService.sendMessage(answer);
                 //@TODO: check for response errors
                 lastConversationMessageId = resultRs.getConversationMessageId();
-                System.out.printf("[%03d] CASE: %s | ANSWER: %s | SLEEPING 12 SECONDS\n",
-                        counter, answer.getType(), answer.getText());
+                if (answer.getType().equals(CaseType.PROFILE) || answer.getType().equals(CaseType.QUESTION_AFTER_PROFILE)) {
+                    if (answer.getText().equals("1")) {
+                        stats.increaseLikes();
+                    } else {
+                        stats.increaseDislikes();
+                    }
+                }
+                System.out.printf("[%03d] (L_%03d/D_%03d) CASE: %s | ANSWER: %s | SLEEPING 12 SECONDS\n",
+                        counter, stats.getLikes(), stats.getDislikes(), answer.getType(), answer.getText());
                 TimeUnit.SECONDS.sleep(12);
             } catch (TooManyLikesForToday e) {
                 isActive = false;
@@ -111,5 +119,24 @@ public class DaiVinchikDialogHandler implements Task {
 }
 
 class Statistic {
+
+    private int likes;
+    private int dislikes;
+
+    public int getLikes() {
+        return likes;
+    }
+
+    public void increaseLikes() {
+        likes++;
+    }
+
+    public int getDislikes() {
+        return dislikes;
+    }
+
+    public void increaseDislikes() {
+        dislikes++;
+    }
 
 }
